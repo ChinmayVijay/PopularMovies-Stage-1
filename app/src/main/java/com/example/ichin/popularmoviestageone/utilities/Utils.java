9ed61cce14d8cc1332e3824998fdb9e468cec299
Chinmay <ichinmay08@gmail.com>
2018-07-31 22:46:35 +0530

    Commit[19]: Stage Two [final commit ] removed api key

--- app/src/main/java/com/example/ichin/popularmoviestageone/utilities/Utils.java
+++ app/src/main/java/com/example/ichin/popularmoviestageone/utilities/Utils.java
@@ -3,7 +3,7 @@ package com.example.ichin.popularmoviestageone.utilities;
 public class Utils {
     public static final String IMAGE_BASE_URL="http://image.tmdb.org/t/p/w185";
     public static final String IMAGE_YOUTUBE_URL = "http://img.youtube.com/vi/";
-    public static final String API_KEY = "5af869fcf0fa026d1847f32746a1f5c2";
+    public static final String API_KEY = "Enter your api_key here";
     public static final int MAX_WIDTH = 1024;
     public static final int MAX_HEIGHT = 768;
 }
